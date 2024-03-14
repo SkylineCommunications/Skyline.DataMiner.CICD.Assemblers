9359d2b39ecf48b04ab9eec0f688650a24ab75cb
Jan Staelens <jan.staelens@skyline.be>
2024-03-14 16:17:33 +0100

    Code cleanup

--- Assemblers.Protocol/AssemblyFilter.cs
+++ Assemblers.Protocol/AssemblyFilter.cs
@@ -1,22 +1,31 @@
-﻿namespace Skyline.DataMiner.CICD.Assemblers.Protocol
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Skyline.DataMiner.CICD.Assemblers.Common;
+using NuGet.Packaging.Core;
+
+namespace Skyline.DataMiner.CICD.Assemblers.Protocol
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
-    using System.Text;
-    using System.IO;
-
-    using Skyline.DataMiner.CICD.Assemblers.Common;
-    using NuGet.Packaging.Core;
-    using System.Threading.Tasks;
-    using Skyline.DataMiner.CICD.Parsers.Common.VisualStudio.Projects;
-
+    /// <summary>
+    /// Provides functionality to filter assemblies based on target framework, package references, and build result items.
+    /// </summary>
     public static class AssemblyFilter
     {
+        /// <summary>
+        /// Filters assemblies based on the specified criteria.
+        /// </summary>
+        /// <param name="targetFrameworkMoniker">The target framework moniker.</param>
+        /// <param name="packageReferenceProcessor">The package reference processor.</param>
+        /// <param name="buildResultItems">The build result items.</param>
+        /// <param name="dllImports">A set of DLL imports.</param>
+        /// <param name="packageIdentities">A list of package identities.</param>
+        /// <returns>The filtered NuGet package assembly data.</returns>
         public static async Task<NuGetPackageAssemblyData> Filter(string targetFrameworkMoniker, PackageReferenceProcessor packageReferenceProcessor, BuildResultItems buildResultItems, HashSet<string> dllImports, IList<PackageIdentity> packageIdentities)
         {
-            NuGetPackageAssemblyData nugetAssemblyData = await packageReferenceProcessor.ProcessAsync(packageIdentities, targetFrameworkMoniker,
+            var nugetAssemblyData = await packageReferenceProcessor.ProcessAsync(packageIdentities, targetFrameworkMoniker,
                 Skyline.DataMiner.CICD.Common.NuGet.DevPackHelper.ProtocolDevPackNuGetDependenciesIncludingTransitive).ConfigureAwait(false);
 
             ProcessFrameworkAssemblies(dllImports, nugetAssemblyData);
@@ -25,53 +34,24 @@
             return nugetAssemblyData;
         }
 
-        private static void ProcessFrameworkAssemblies(HashSet<string> dllImports, NuGetPackageAssemblyData nugetAssemblyData)
-        {
-            foreach (var frameworkAssembly in nugetAssemblyData.DllImportFrameworkAssemblyReferences)
-            {
-                if (!Helper.QActionDefaultImportDLLs.Any(a => String.Equals(a, frameworkAssembly, StringComparison.OrdinalIgnoreCase)))
-                {
-                    dllImports.Add(frameworkAssembly);
-                }
-            }
-        }
-
-        private static void ProcessLibAssemblies(BuildResultItems buildResultItems, HashSet<string> dllImports, NuGetPackageAssemblyData nugetAssemblyData)
-        {
-            ProcessDllImportNuGetAssemblyReferences(dllImports, nugetAssemblyData);
-
-            foreach (var dir in nugetAssemblyData.DllImportDirectoryReferences)
-            {
-                if (!Helper.QActionDefaultImportDLLs.Any(d => String.Equals(d, dir, StringComparison.OrdinalIgnoreCase)))
-                {
-                    dllImports.Add(dir);
-                }
-            }
-
-            foreach (var libItem in nugetAssemblyData.NugetAssemblies)
-            {
-                // It's possible that frameworkassemblies like System.Net.Http exist and were added as a dependency earlier on.
-                // If that's the case, we should ignore the NuGet adding the same thing.
-                // "System.Net.Http.dll" for example.
-                // Need to check dllImports
-
-                if (!dllImports.Contains(Path.GetFileName(libItem.AssemblyPath)) && buildResultItems.Assemblies.FirstOrDefault(b => b.AssemblyPath == libItem.AssemblyPath) == null)
-                {
-                    buildResultItems.Assemblies.Add(libItem);
-                }
-            }
-        }
-
+        /// <summary>
+        /// Adds a new DLL import to the set if it does not already contain an import with the same name.
+        /// </summary>
+        /// <param name="dllImports">A set of DLL imports.</param>
+        /// <param name="newImport">The new import to add.</param>
         private static void AddToDllImport(HashSet<string> dllImports, string newImport)
         {
-            // Only add a dll import that is not the same name of the dll. This avoids duplicates.
-
             if (!dllImports.Select(p => Path.GetFileName(p)).Contains(Path.GetFileName(newImport)))
             {
                 dllImports.Add(newImport);
             }
         }
 
+        /// <summary>
+        /// Processes NuGet assembly references for DLL import and updates the set of DLL imports and directory references accordingly.
+        /// </summary>
+        /// <param name="dllImports">A set of DLL imports.</param>
+        /// <param name="nugetAssemblyData">The NuGet package assembly data.</param>
         private static void ProcessDllImportNuGetAssemblyReferences(HashSet<string> dllImports, NuGetPackageAssemblyData nugetAssemblyData)
         {
             if (nugetAssemblyData.DllImportNugetAssemblyReferences.Count <= 0)
@@ -79,72 +59,49 @@
                 return;
             }
 
-            HashSet<string> directoriesWithExplicitDllImport = new HashSet<string>();
-            List<string> potentialRemainingDirectoryImports = new List<string>();
-
-            // At this point it could be that there are multiple assemblies with the same name (if different NuGet packages contains the same assembly).
-            // If this is the case, we select the highest version.
-            Dictionary<string, List<PackageAssemblyReference>> assemblies = new Dictionary<string, List<PackageAssemblyReference>>();
+            var directoriesWithExplicitDllImport = new HashSet<string>();
+            var potentialRemainingDirectoryImports = new List<string>();
+            var assemblies = new Dictionary<string, List<PackageAssemblyReference>>();
 
             foreach (var libItem in nugetAssemblyData.DllImportNugetAssemblyReferences)
             {
                 string assemblyName = Path.GetFileName(libItem.DllImport);
 
-                if (assemblies.TryGetValue(assemblyName, out List<PackageAssemblyReference> entries))
+                if (assemblies.TryGetValue(assemblyName, out var entries))
                 {
                     entries.Add(libItem);
                 }
                 else
                 {
-                    assemblies.Add(assemblyName, new List<PackageAssemblyReference> { libItem });
+                    assemblies[assemblyName] = new List<PackageAssemblyReference> { libItem };
                 }
             }
 
-            foreach (var assembly in assemblies.Keys)
+            foreach (var assembly in assemblies)
             {
-                var packagesContainingAssembly = assemblies[assembly];
+                var packagesContainingAssembly = assembly.Value;
 
                 if (packagesContainingAssembly.Count == 1)
                 {
                     var libItem = packagesContainingAssembly[0];
-
                     AddToDllImport(dllImports, libItem.DllImport);
-                    directoriesWithExplicitDllImport.Add(libItem.DllImport.Substring(0, libItem.DllImport.Length - assembly.Length));
+                    directoriesWithExplicitDllImport.Add(libItem.DllImport.Substring(0, libItem.DllImport.Length - assembly.Key.Length));
                 }
                 else
                 {
-                    Version mostRecentVersion = null;
-                    PackageAssemblyReference selectedItem = null;
-
-                    // Select most recent version.
-                    foreach (var libItem in packagesContainingAssembly)
+                    PackageAssemblyReference mostRecentLibItem = SelectMostRecentVersion(packagesContainingAssembly);
+                    if (mostRecentLibItem != null)
                     {
-                        var version = AssemblyName.GetAssemblyName(libItem.AssemblyPath).Version;
+                        AddToDllImport(dllImports, mostRecentLibItem.DllImport);
+                        directoriesWithExplicitDllImport.Add(mostRecentLibItem.DllImport.Substring(0, mostRecentLibItem.DllImport.Length - assembly.Key.Length));
 
-                        if (mostRecentVersion == null || version > mostRecentVersion)
+                        foreach (var libItem in packagesContainingAssembly)
                         {
-                            mostRecentVersion = version;
-                            selectedItem = libItem;
-                        }
-                    }
-
-                    if (selectedItem == null)
-                    {
-                        continue;
-                    }
-
-                    AddToDllImport(dllImports, selectedItem.DllImport);
-                    directoriesWithExplicitDllImport.Add(selectedItem.DllImport.Substring(0,
-                        selectedItem.DllImport.Length - assembly.Length));
-
-                    // Add other items that were not selected as hint directories.
-                    foreach (var libItem in packagesContainingAssembly)
-                    {
-                        if (libItem != selectedItem)
-                        {
-                            string directoryPath = libItem.DllImport.Substring(0, libItem.DllImport.Length - assembly.Length);
-
-                            potentialRemainingDirectoryImports.Add(directoryPath);
+                            if (libItem != mostRecentLibItem)
+                            {
+                                string directoryPath = libItem.DllImport.Substring(0, libItem.DllImport.Length - assembly.Key.Length);
+                                potentialRemainingDirectoryImports.Add(directoryPath);
+                            }
                         }
                     }
                 }
@@ -159,5 +116,70 @@
             }
         }
 
+        /// <summary>
+        /// Processes framework assemblies and adds them to the set of DLL imports if not already present.
+        /// </summary>
+        /// <param name="dllImports">A set of DLL imports.</param>
+        /// <param name="nugetAssemblyData">The NuGet package assembly data.</param>
+        private static void ProcessFrameworkAssemblies(HashSet<string> dllImports, NuGetPackageAssemblyData nugetAssemblyData)
+        {
+            foreach (var frameworkAssembly in nugetAssemblyData.DllImportFrameworkAssemblyReferences)
+            {
+                if (!Helper.QActionDefaultImportDLLs.Any(a => string.Equals(a, frameworkAssembly, StringComparison.OrdinalIgnoreCase)))
+                {
+                    dllImports.Add(frameworkAssembly);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Processes library assemblies and updates the build result items and DLL imports accordingly.
+        /// </summary>
+        /// <param name="buildResultItems">The build result items.</param>
+        /// <param name="dllImports">A set of DLL imports.</param>
+        /// <param name="nugetAssemblyData">The NuGet package assembly data.</param>
+        private static void ProcessLibAssemblies(BuildResultItems buildResultItems, HashSet<string> dllImports, NuGetPackageAssemblyData nugetAssemblyData)
+        {
+            ProcessDllImportNuGetAssemblyReferences(dllImports, nugetAssemblyData);
+
+            foreach (var dir in nugetAssemblyData.DllImportDirectoryReferences)
+            {
+                if (!Helper.QActionDefaultImportDLLs.Any(d => string.Equals(d, dir, StringComparison.OrdinalIgnoreCase)))
+                {
+                    dllImports.Add(dir);
+                }
+            }
+
+            foreach (var libItem in nugetAssemblyData.NugetAssemblies)
+            {
+                if (!dllImports.Contains(Path.GetFileName(libItem.AssemblyPath)) && buildResultItems.Assemblies.FirstOrDefault(b => b.AssemblyPath == libItem.AssemblyPath) == null)
+                {
+                    buildResultItems.Assemblies.Add(libItem);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Selects the most recent version of a package assembly reference from a list.
+        /// </summary>
+        /// <param name="packagesContainingAssembly">The list of package assembly references.</param>
+        /// <returns>The most recent package assembly reference, or null if the list is empty.</returns>
+        private static PackageAssemblyReference SelectMostRecentVersion(List<PackageAssemblyReference> packagesContainingAssembly)
+        {
+            PackageAssemblyReference mostRecentLibItem = null;
+            Version mostRecentVersion = null;
+
+            foreach (var libItem in packagesContainingAssembly)
+            {
+                var version = AssemblyName.GetAssemblyName(libItem.AssemblyPath).Version;
+                if (mostRecentVersion == null || version > mostRecentVersion)
+                {
+                    mostRecentVersion = version;
+                    mostRecentLibItem = libItem;
+                }
+            }
+
+            return mostRecentLibItem;
+        }
     }
 }
