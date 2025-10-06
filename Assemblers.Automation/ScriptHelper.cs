3a5ce1aee4493834291d64d05188dbf8a7e6627b
Michiel Oda <97458010+MichielOda@users.noreply.github.com>
2025-10-06 09:10:07 +0200

    ProcessAutomation.dll is in ProtocolScripts directory (#96)
    * ProcessAutomation.dll is in ProtocolScripts directory
    
    * Removing .NET 6 from the unit test projects

--- Assemblers.Automation/ScriptHelper.cs
+++ Assemblers.Automation/ScriptHelper.cs
@@ -38,6 +38,7 @@
         public static readonly string[] DllsWithProtocolScriptsPath =
         {
             "DataMinerSolutions.dll",
+            "ProcessAutomation.dll"
         };
 
         /// <summary>
